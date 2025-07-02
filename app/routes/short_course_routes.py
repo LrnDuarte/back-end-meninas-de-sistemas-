911455893899a63dde51b6764d4b0cea8dd5e65b
Leandro-Coutinhodev <leandrocoutinho.dev@gmail.com>
2025-07-01 23:21:43 -0300

    Adicionando rotas

--- app/routes/short_course_routes.py
+++ app/routes/short_course_routes.py
@@ -1,7 +1,10 @@
 from flask import Blueprint
+from app.controllers import short_course_controller
 
-short_course_bp = Blueprint('short_course', __name__)
+short_course_bp = Blueprint('short_course_bp', __name__, url_prefix='/short_course')
 
-@short_course_bp.route('/short-courses')
-def list_short_courses():
-    return {"msg": "Lista de cursos rápidos"}
+short_course_bp.route('', methods=['GET'])(short_course_controller.get_all_short_course)
+short_course_bp.route('/<int:id_short_course>', methods=['GET'])(short_course_controller.get_short_course_by_id)
+short_course_bp.route('', methods=['POST'])(short_course_controller.create_short_course)
+short_course_bp.route('/<int:id_short_course>', methods=['PUT'])(short_course_controller.update_short_course)
+short_course_bp.route('/<int:id_short_course>', methods=['DELETE'])(short_course_controller.delete_short_course)
--- app/routes/workshop_routes.py
+++ app/routes/workshop_routes.py
@@ -1,7 +1,10 @@
 from flask import Blueprint
+from app.controllers import workshop_controller
 
-workshop_bp = Blueprint('workshop', __name__)
+workshop_bp = Blueprint('workshop_bp', __name__, url_prefix='/workshop')
 
-@workshop_bp.route('/workshops')
-def list_workshops():
-    return {"msg": "Lista de workshops"}
+workshop_bp.route('', methods=['GET'])(workshop_controller.get_all_workshop)
+workshop_bp.route('/<int:id_workshop>', methods=['GET'])(workshop_controller.get_workshop_by_id)
+workshop_bp.route('', methods=['POST'])(workshop_controller.create_worhshop)
+workshop_bp.route('/<int:id_workshop>', methods=['PUT'])(workshop_controller.update_workshop)
+workshop_bp.route('/<int:id_workshop>', methods=['DELETE'])(workshop_controller.delete_workshop)
